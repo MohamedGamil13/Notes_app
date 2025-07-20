11729a1c75d3e4c1e0d625a8f86d7398f8a14fde
Mohamed Gamil <mohamedgamil0661672@gmail.com>
2025-07-20 21:22:04 +0300

    Add Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/pages/homepage.dart';
+import 'package:notes_app/widgets/constansts.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(KBox);
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
--- lib/widgets/constansts.dart
+++ lib/widgets/constansts.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color KprimaryColor = Color(0xff62fcd7);
+const String KBox = 'Notes_Box';
